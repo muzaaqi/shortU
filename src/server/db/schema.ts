3f9fdd17a2d1c7c3e6471562e6082e8a0fe5ab04
MUZAAQI <muzzaaqi@gmail.com>
2026-08-22 09:26:59 +0700

    fix(db): add missing issuer column to accounts table in schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -34,6 +34,7 @@ export const sessions = pgTable("sessions", {
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
+  issuer: text("issuer").notNull(),
   accountId: text("account_id").notNull(),
   providerId: text("provider_id").notNull(),
   userId: text("user_id")
